7329b1198a71d20838199b5049351a1d44e54f63
Pavel Kovář <kovarp95@gmail.com>
2019-04-07 18:14:20 +0200

    Errors handling added

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
 
+/**
+ * Error codes:
+ * 0 - no error
+ * 1 - invalid auth code
+ * 2 - cannot connect to database
+ */
+
+error_reporting(0);
+
+$data = [];
+
+$data['error'] = 0;
+
 if (!isset($_GET['auth']) || empty($_GET['auth']) || $_GET['auth'] !== '') {
-	die('Invalid auth token');
+	$data['error'] = 1;
+	$data['errorCode'] = 1;
+} else {
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	$worker = new \kovarp\DbBackupWorker\Worker(
+		'',
+		'',
+		'',
+		'localhost',
+		''
+	);
+
+	$worker->dump();
+
+	if ($worker->hasError()) {
+		$data['error'] = 1;
+		$data['errorCode'] = $worker->getErrorCode();
+	}
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-$worker = new \kovarp\DbBackupWorker\Worker(
-	'',
-	'',
-	'',
-	'localhost',
-	''
-);
-
-$worker->dump();
+header('Content-type: application/json');
+echo json_encode( $data );
--- src/Worker.php
+++ src/Worker.php
@@ -14,6 +14,8 @@ class Worker {
 
 	private $secret;
 
+	private $errorCode = 0;
+
 	public function __construct(string $name, string $user, string $password, string $host, string $secret) {
 		$this->name = $name;
 		$this->user = $user;
@@ -30,8 +32,24 @@ class Worker {
 			$this->name
 		);
 
-		$dump = new \MySQLDump($db);
+		if ($db->connect_errno) {
+			$this->setErrorCode(2);
+		} else {
+			$dump = new \MySQLDump($db);
 
-		$dump->save(__DIR__ . '/../' . sha1('dump' . $this->secret) . '.sql');
+			$dump->save(__DIR__ . '/../' . sha1('dump' . $this->secret) . '.sql');
+		}
+	}
+
+	public function hasError() {
+		return ($this->errorCode !== 0);
+	}
+
+	public function setErrorCode($errorCode) {
+		$this->errorCode = $errorCode;
+	}
+
+	public function getErrorCode() {
+		return $this->errorCode;
 	}
 }
